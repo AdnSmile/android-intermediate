0c3027e47e5bcfb1e31d45e5207090c4366b43f5
AdnSmile <nessafitri16@gmail.com>
2023-04-27 23:44:56 +0700

    Latihan Firebase Realtime Database

--- firebase/LatihanFirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ firebase/LatihanFirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FirebaseMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +37,49 @@ class MainActivity : AppCompatActivity() {
             finish()
             return
         }
+
+        db = Firebase.database // inisialisasi database
+
+        val messageRef = db.reference.child(MESSAGES_CHILD) // menentukan lokasi data
+
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = Message(
+                binding.messageEditText.text.toString(),
+                firebaseUser.displayName.toString(),
+                firebaseUser.photoUrl.toString(),
+                Date().time
+            )
+
+            // menambah atau mengganti data
+            messageRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(this, getString(R.string.send_error) + error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT).show()
+                }
+            }
+            binding.messageEditText.setText("")
+        }
+
+        val manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+
+        val option = FirebaseRecyclerOptions.Builder<Message>()
+            .setQuery(messageRef, Message::class.java)
+            .build()
+        adapter = FirebaseMessageAdapter(option, firebaseUser.displayName)
+        binding.messageRecyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.startListening()
+    }
+
+    override fun onPause() {
+        adapter.stopListening()
+        super.onPause()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -51,5 +102,9 @@ class MainActivity : AppCompatActivity() {
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    companion object {
+        const val MESSAGES_CHILD = "messages"
     }
 }
